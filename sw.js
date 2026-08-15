67c61811839a2a7b648b89f46afaa623438b3b13
Tonbi <tonbaratiminipredestiny@gmail.com>
2026-08-15 06:43:14 +0100

    Add service worker for caching and offline support

---
+++ sw.js
@@ -0,0 +1,69 @@
+const CACHE_NAME = 'neon-bay-v1';
+const CORE_ASSETS = [
+    './',
+    './index.html',
+    './css/main.css',
+    './js/main.js',
+    './js/core/Game.js',
+    './js/core/SceneManager.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js'
+];
+
+// Install
+self.addEventListener('install', event => {
+    console.log('[SW] Install');
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('[SW] Caching core assets');
+                return cache.addAll(CORE_ASSETS);
+            })
+            .then(() => self.skipWaiting())
+    );
+});
+
+// Activate
+self.addEventListener('activate', event => {
+    console.log('[SW] Activate');
+    event.waitUntil(
+        caches.keys()
+            .then(cacheNames => {
+                return Promise.all(
+                    cacheNames
+                        .filter(name => name !== CACHE_NAME)
+                        .map(name => caches.delete(name))
+                );
+            })
+            .then(() => self.clients.claim())
+    );
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request)
+                    .then(response => {
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        const responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+                        return response;
+                    });
+            })
+            .catch(() => {
+                // Offline fallback
+                if (event.request.mode === 'navigate') {
+                    return caches.match('./index.html');
+                }
+            })
+    );
+});
